0d94cf81c2f331b224e577189656060982e5c07b
Sebastian <seblerr@gmail.com>
2023-11-14 12:28:15 +0100

    add wezterm dot

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+local wezterm = require 'wezterm'
+
+return {
+  -- General settings
+  color_scheme = 'catppuccin-macchiato',
+
+  font = wezterm.font_with_fallback({
+    { family = "JetBrains Mono",         weight = 'Medium' },
+    { family = "Symbols Nerd Font Mono", scale = 0.67 }
+  }),
+  font_size = 14,
+
+  enable_tab_bar = false,
+
+  window_padding = {
+    left = 2,
+    right = 2,
+    top = 0,
+    bottom = 0,
+  },
+  window_decorations = "RESIZE",
+  window_close_confirmation = "NeverPrompt",
+
+  audible_bell = "Disabled",
+
+}
