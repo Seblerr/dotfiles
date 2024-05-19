228fa1bfee371f7adef3433dbec82c7e68fba122
Sebastian <seblerr@gmail.com>
2024-05-19 10:25:40 +0200

    fix luasnip and debug binds

--- neovim/.config/nvim/lua/seba/plugins/debug.lua
+++ neovim/.config/nvim/lua/seba/plugins/debug.lua
@@ -1,14 +1,14 @@
 return {
   'mfussenegger/nvim-dap',
-  keys = {
-    { "<leader>du", function() require("dapui").toggle() end,               desc = "Dap UI" },
-    { "<leader>dr", function() require("dapui").open({ reset = true }) end, desc = "Reset Dap UI" },
-    { "<leader>de", function() require("dapui").eval() end,                 desc = "Eval",        mode = { "n", "v" } },
-  },
   dependencies = {
     {
       'rcarriga/nvim-dap-ui',
       dependencies = { "nvim-neotest/nvim-nio" },
+      keys = {
+        { "<leader>du", function() require("dapui").toggle() end,               desc = "Dap UI" },
+        { "<leader>dr", function() require("dapui").open({ reset = true }) end, desc = "Reset Dap UI" },
+        { "<leader>de", function() require("dapui").eval() end,                 desc = "Eval",        mode = { "n", "v" } },
+      },
       config = function()
         local dap = require('dap')
         local dapui = require('dapui')
@@ -34,12 +34,14 @@ return {
       opts = {},
     },
   },
+  keys = {
+    { "<leader>dc", function() require('dap').continue() end, "Start/continue debug" }
+  },
   config = function()
     local dap = require 'dap'
     local launch_path = require('seba.util').get_git_root() .. '/.vscode/launch.json'
 
     -- Keymaps
-    vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Debug: Start/Continue' })
     vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<leader>do', dap.step_out, { desc = 'Debug: Step Out' })
     vim.keymap.set('n', '<leader>dj', dap.down, { desc = 'Debug: Down' })
--- neovim/.config/nvim/lua/seba/plugins/nvim-cmp.lua
+++ neovim/.config/nvim/lua/seba/plugins/nvim-cmp.lua
@@ -24,6 +24,7 @@ return
     opts = function()
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
       local defaults = require("cmp.config.default")()
 
       return {
