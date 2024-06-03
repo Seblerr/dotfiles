a29de9e74d95a2906c2fb2fb1eaef8a1cac12497
Sebastian Nilsson <seblerr@gmail.com>
2024-06-03 12:01:07 +0200

    mini.git configuration

--- neovim/.config/nvim/lua/seba/plugins/mini.lua
+++ neovim/.config/nvim/lua/seba/plugins/mini.lua
@@ -78,6 +78,34 @@ return {
     'echasnovski/mini-git',
     version = false,
     main = 'mini.git',
-    opts = {},
+    opts = {
+      command = {
+        split = 'vertical',
+      },
+    },
+    config = function(_, opts)
+      local rhs = '<Cmd>lua MiniGit.show_at_cursor()<CR>'
+      vim.keymap.set({ 'n', 'x' }, '<Leader>gs', rhs, { desc = 'Show at cursor' })
+      vim.keymap.set({ 'n', 'x' }, '<Leader>gb', '<Cmd>vertical Git blame -- %<CR>', { desc = 'Git blame buffer' })
+
+      local align_blame = function(au_data)
+        if au_data.data.git_subcommand ~= 'blame' then return end
+
+        -- Align blame output with source
+        local win_src = au_data.data.win_source
+        vim.wo.wrap = false
+        vim.fn.winrestview({ topline = vim.fn.line('w0', win_src) })
+        vim.api.nvim_win_set_cursor(0, { vim.fn.line('.', win_src), 0 })
+
+        -- Bind both windows so that they scroll together
+        vim.wo[win_src].scrollbind, vim.wo.scrollbind = true, true
+      end
+
+      local au_opts = { pattern = 'MiniGitCommandSplit', callback = align_blame }
+      vim.api.nvim_create_autocmd('User', au_opts)
+
+      require('mini.git').setup(opts)
+    end
+
   },
 }
