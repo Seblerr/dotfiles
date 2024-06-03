dd67229668dc8a34874b9dfebe3cb4c2eb7fb6f9
Sebastian Nilsson <seblerr@gmail.com>
2024-06-03 20:52:26 +0200

    small changes to mini plugins

--- neovim/.config/nvim/lua/seba/plugins/mini.lua
+++ neovim/.config/nvim/lua/seba/plugins/mini.lua
@@ -23,6 +23,15 @@ return {
   },
 
   {
+    'echasnovski/mini.pick',
+    event = "VeryLazy",
+    version = false,
+    config = function()
+      require('mini.pick').setup()
+    end
+  },
+
+  {
     'echasnovski/mini.move',
     event = { "BufReadPost", "BufNewFile" },
     version = false,
@@ -54,6 +63,31 @@ return {
   },
 
   {
+    "echasnovski/mini.bufremove",
+    keys = {
+      {
+        "<leader>bd",
+        function()
+          local bd = require("mini.bufremove").delete
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+            if choice == 1 then -- Yes
+              vim.cmd.write()
+              bd(0)
+            elseif choice == 2 then -- No
+              bd(0, true)
+            end
+          else
+            bd(0)
+          end
+        end,
+        desc = "Delete Buffer",
+      },
+      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+    }
+  },
+
+  {
     'echasnovski/mini.files',
     version = '*',
     keys = {
@@ -76,6 +110,7 @@ return {
 
   {
     'echasnovski/mini-git',
+    event = "VeryLazy",
     version = false,
     main = 'mini.git',
     opts = {
--- neovim/.config/nvim/lua/seba/plugins/ui.lua
+++ neovim/.config/nvim/lua/seba/plugins/ui.lua
@@ -70,37 +70,4 @@ return {
       vim.notify = require("notify")
     end,
   },
-
-  {
-    "echasnovski/mini.bufremove",
-    keys = {
-      {
-        "<leader>bd",
-        function()
-          local bd = require("mini.bufremove").delete
-          if vim.bo.modified then
-            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
-            if choice == 1 then -- Yes
-              vim.cmd.write()
-              bd(0)
-            elseif choice == 2 then -- No
-              bd(0, true)
-            end
-          else
-            bd(0)
-          end
-        end,
-        desc = "Delete Buffer",
-      },
-      { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
-    }
-  },
-
-  {
-    'echasnovski/mini.pick',
-    version = false,
-    config = function()
-      require('mini.pick').setup()
-    end
-  },
 }
