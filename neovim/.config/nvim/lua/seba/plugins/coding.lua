3aee51bef5cd8ca18f247c725a7ceb292ba32d03
Sebastian Nilsson <seblerr@gmail.com>
2024-06-28 16:19:46 +0200

    keybinds

--- neovim/.config/nvim/lua/seba/plugins/coding.lua
+++ neovim/.config/nvim/lua/seba/plugins/coding.lua
@@ -84,7 +84,7 @@ return {
   {
     "Wansmer/treesj",
     keys = {
-      { "J", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+      { "<leader>m", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
     },
     opts = { use_default_keymaps = false, max_join_length = 150 },
   },
