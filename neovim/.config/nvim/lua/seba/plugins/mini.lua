01db266d2db9ae866c7ac02b11fc1639381043c3
Sebastian Nilsson <seblerr@gmail.com>
2024-07-03 15:07:05 +0200

    add split to mini.files

--- neovim/.config/nvim/lua/seba/plugins/mini.lua
+++ neovim/.config/nvim/lua/seba/plugins/mini.lua
@@ -106,7 +106,33 @@ return {
         end
       end, "MiniFiles open" },
     },
-    opts = {}
+    config = function()
+      local map_split = function(buf_id, lhs, direction)
+        local rhs = function()
+          -- Make new window and set it as target
+          local new_target_window
+          vim.api.nvim_win_call(require('mini.files').get_target_window(), function()
+            vim.cmd(direction .. ' split')
+            new_target_window = vim.api.nvim_get_current_win()
+          end)
+
+          MiniFiles.set_target_window(new_target_window)
+        end
+
+        -- Adding `desc` will result into `show_help` entries
+        local desc = 'Split ' .. direction
+        vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+      end
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          map_split(buf_id, 'gs', 'belowright vertical')
+        end,
+      })
+      require('mini.files').setup()
+    end
   },
 
   {
