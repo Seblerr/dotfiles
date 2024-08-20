c89a38348d3edb9cbb4b00252f645c85afe95a15
Sebastian Nilsson <seblerr@gmail.com>
2024-08-20 14:00:03 +0200

    minor changes

--- neovim/.config/nvim/lua/seba/plugins/mini.lua
+++ neovim/.config/nvim/lua/seba/plugins/mini.lua
@@ -222,8 +222,8 @@ return {
       require('mini.misc').setup({
         make_global = { "put", "put_text", "zoom" }
       })
-      MiniMisc.setup_auto_root()
-      -- MiniMisc.setup_termbg_sync()
+      -- MiniMisc.setup_auto_root()
+      MiniMisc.setup_termbg_sync()
       MiniMisc.setup_restore_cursor()
     end
   }
