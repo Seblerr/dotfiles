699e7c4915e6c71f9dcc0c458e6bda6eefdc669d
Sebastian Nilsson <seblerr@gmail.com>
2024-10-24 12:36:16 +0200

    add i3 config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ return {
   color_scheme = 'catppuccin-macchiato',
 
   font = wezterm.font_with_fallback({
-    { family = "JetBrainsMono NF",       weight = 'Medium' },
+    { family = "JetBrainsMono Nerd Font",       weight = 'Medium' },
     { family = "Symbols Nerd Font Mono", scale = 0.67 }
   }),
   font_size = 14,
