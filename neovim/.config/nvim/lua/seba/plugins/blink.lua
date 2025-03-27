f5bddb6d17486364483905f8335cd37ab6fff7f1
Sebastian Nilsson <seblerr@gmail.com>
2025-03-27 09:12:49 +0100

    update blink config

--- neovim/.config/nvim/lua/seba/plugins/blink.lua
+++ neovim/.config/nvim/lua/seba/plugins/blink.lua
@@ -3,6 +3,9 @@ return {
   dependencies = 'rafamadriz/friendly-snippets',
   version = '*',
   opts = {
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
     enabled = function()
       return not vim.list_contains({ 'DressingInput' }, vim.bo.filetype)
           and vim.bo.buftype ~= 'prompt'
@@ -14,6 +17,7 @@ return {
     appearance = {
       nerd_font_variant = 'mono'
     },
+    fuzzy = { implementation = "prefer_rust_with_warning" }
   },
   opts_extend = { "sources.default" }
 }
