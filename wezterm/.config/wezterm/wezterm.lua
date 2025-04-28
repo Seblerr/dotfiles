d78eecd9f4c6d52983523c77f7c922d98539a5e9
Sebastian Nilsson <seblerr@gmail.com>
2025-04-28 11:57:06 +0200

    update wezterm and i3 config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,30 +1,30 @@
 local wezterm = require 'wezterm'
 
 return {
-  color_scheme = 'catppuccin-macchiato',
-
-  font = wezterm.font_with_fallback({
-    { family = "JetBrainsMono Nerd Font", weight = 'Medium' },
-    { family = "Symbols Nerd Font Mono",  scale = 0.67 }
-  }),
+  color_scheme = 'catppuccin-mocha',
   font_size = 14,
+
+  window_padding = {
+    left = '0.3cell',
+    right = '0.3cell',
+    top = '0.25cell',
+    bottom = '0.25cell',
+  },
 
   audible_bell = "Disabled",
   enable_tab_bar = false,
   window_decorations = "RESIZE",
   window_close_confirmation = "NeverPrompt",
-
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
-
   max_fps = 120,
-  animation_fps = 120,
 
+
+  disable_default_key_bindings = true,
   keys = {
+    { key = '+', mods = 'CTRL',       action = wezterm.action.IncreaseFontSize },
+    { key = '-', mods = 'CTRL',       action = wezterm.action.DecreaseFontSize },
+    { key = '0', mods = 'CTRL',       action = wezterm.action.ResetFontSize },
+    { key = 'c', mods = 'SHIFT|CTRL', action = wezterm.action.CopyTo 'Clipboard' },
+    { key = 'v', mods = 'SHIFT|CTRL', action = wezterm.action.PasteFrom 'Clipboard' },
     {
       key = 'Backspace',
       mods = 'CTRL',
