eb032c45667f137e21d979637231d5472ab58a6d
Sebastian Nilsson <seblerr@gmail.com>
2025-09-05 13:06:46 +0200

    nvim: remove neocodeium

--- neovim/.config/nvim/lua/seba/plugins/ai.lua
+++ neovim/.config/nvim/lua/seba/plugins/ai.lua
@@ -1,42 +1,5 @@
 return {
   {
-    "monkoose/neocodeium",
-    enabled = false,
-    opts = {
-      filetypes = {
-        ["dap-repl"] = false,
-        dapui_watches = false,
-        DressingInput = false,
-        snacks_picker_input = false,
-      }
-    },
-    config = function(_, opts)
-      local neocodeium = require("neocodeium")
-
-      vim.keymap.set("i", "<A-f>", function()
-        neocodeium.accept()
-      end)
-      vim.keymap.set("i", "<A-w>", function()
-        neocodeium.accept_word()
-      end)
-      vim.keymap.set("i", "<A-a>", function()
-        neocodeium.accept_line()
-      end)
-      vim.keymap.set("i", "<A-e>", function()
-        neocodeium.cycle_or_complete()
-      end)
-      vim.keymap.set("i", "<A-q>", function()
-        neocodeium.cycle_or_complete(-1)
-      end)
-      vim.keymap.set("i", "<A-c>", function()
-        neocodeium.clear()
-      end)
-
-      neocodeium.setup(opts)
-    end,
-  },
-
-  {
     "olimorris/codecompanion.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -53,5 +16,4 @@ return {
       },
     },
   },
-
 }
