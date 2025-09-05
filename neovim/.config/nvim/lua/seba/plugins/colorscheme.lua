1cd5d3016b56c32c939aefb7dbd76823cbd7d474
Sebastian Nilsson <seblerr@gmail.com>
2025-09-05 13:09:37 +0200

    nvim: cleanup colorscheme

--- neovim/.config/nvim/lua/seba/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/seba/plugins/colorscheme.lua
@@ -1,14 +1,4 @@
 return {
-  -- {
-  --   -- Theme inspired by Atom
-  --   'navarasu/onedark.nvim',
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme 'onedark'
-  --   end,
-  -- },
-  --
-
   {
     "catppuccin/nvim",
     lazy = false,
@@ -22,20 +12,4 @@ return {
       vim.cmd([[colorscheme catppuccin]])
     end,
   },
-
-  -- {
-  --   "ellisonleao/gruvbox.nvim",
-  --   priority = 1000,
-  --   opts = {
-  --     overrides = {
-  --       -- Add highlight for vim-illuminate instead of underline
-  --       IlluminatedWordText = { bg = "#3c3836" },
-  --       IlluminatedWordRead = { bg = "#3c3836" },
-  --       IlluminatedWordWrite = { bg = "#3c3836" },
-  --     },
-  --   },
-  --   config = function()
-  --     vim.cmd.colorscheme 'gruvbox'
-  --   end,
-  -- }
 }
