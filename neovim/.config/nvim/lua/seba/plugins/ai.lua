ad60bffe44bda70e2435cba7554909dfdd340803
Sebastian Nilsson <seblerr@gmail.com>
2025-11-19 14:47:37 +0100

    update sidekick config

--- neovim/.config/nvim/lua/seba/plugins/ai.lua
+++ neovim/.config/nvim/lua/seba/plugins/ai.lua
@@ -1,84 +1,57 @@
 return {
-  -- {
-  --   "olimorris/codecompanion.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "nvim-treesitter/nvim-treesitter",
-  --   },
-  --   opts = {
-  --     strategies = {
-  --       chat = {
-  --         adapter = "gemini",
-  --       },
-  --       inline = {
-  --         adapter = "gemini",
-  --       },
-  --     }
-  --   }
-  -- },
-
   {
     "folke/sidekick.nvim",
-    -- dependencies = {
-    --   { "zbirenbaum/copilot.lua", opts = {}, cmd = "Copilot" }
-    -- },
     opts = {
-      -- nes = {
-      --   enabled = false
-      -- },
+      nes = {
+        enabled = false
+      },
       cli = {
         mux = {
           backend = "tmux",
           enabled = true,
         },
-        tools = {
-          gemini = { cmd = { "gemini" }, url = "https://github.com/google-gemini/gemini-cli" },
-        }
       },
     },
     keys = {
       {
-        "<tab>",
-        function()
-          if require("sidekick").nes_jump_or_apply() then
-            return
-          end
-
-          -- if you are using Neovim's native inline completions
-          if vim.lsp.inline_completion.get() then
-            return
-          end
-
-          return "<tab>"
-        end,
-        mode = { "i", "n" },
-        expr = true,
-        desc = "Goto/Apply Next Edit Suggestion",
+        "<leader>aa",
+        function() require("sidekick.cli").toggle() end,
+        desc = "Sidekick Toggle CLI",
+      },
+      {
+        "<leader>as",
+        function() require("sidekick.cli").select({ filter = { installed = true } }) end,
+        desc = "Select CLI",
       },
       {
         "<leader>at",
-        function()
-          require("sidekick.cli").focus()
-        end,
+        function() require("sidekick.cli").send({ msg = "{this}" }) end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        "<leader>av",
+        function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+        mode = { "x" },
+        desc = "Send Visual Selection",
+      },
+      {
+        "<leader>ap",
+        function() require("sidekick.cli").prompt() end,
+        mode = { "n", "x" },
+        desc = "Sidekick Select Prompt",
+      },
+      {
+        "<c-,>",
+        function() require("sidekick.cli").focus() end,
         mode = { "n", "x", "i", "t" },
         desc = "Sidekick Switch Focus",
       },
       {
-        "<leader>ag",
-        function()
-          require("sidekick.cli").toggle({ name = "gemini", focus = true })
-        end,
-        desc = "Sidekick Gemini Toggle",
-        mode = { "n", "v" },
+        "<leader>aq",
+        function() require("sidekick.cli").toggle({ name = "amazon_q", focus = true }) end,
+        desc = "Sidekick Toggle Claude",
       },
-      {
-        "<leader>ap",
-        function()
-          require("sidekick.cli").select_prompt()
-        end,
-        desc = "Sidekick Ask Prompt",
-        mode = { "n", "v" },
-      },
-    },
-  }
+    }
+  },
 }
