f3148101121fe39abe5c28bd219aa95450cf005d
Sebastian Nilsson <seblerr@gmail.com>
2026-01-15 11:39:50 +0100

    nvim: fzflua updates

--- neovim/.config/nvim/lua/seba/plugins/fzf.lua
+++ neovim/.config/nvim/lua/seba/plugins/fzf.lua
@@ -1,8 +1,8 @@
 return {
   "ibhagwan/fzf-lua",
   keys = {
-    { "<leader><leader>", "<Cmd>FzfLua files<CR>",            desc = "FzfLua files" },
-    { "<leader>ff", "<Cmd>FzfLua files<CR>",                  desc = "FzfLua files" },
+    { "<leader><leader>", "<Cmd>FzfLua files<CR>", desc = "FzfLua files" },
+    { "<leader>ff",       "<Cmd>FzfLua files<CR>", desc = "FzfLua files" },
     {
       "<leader>fF",
       function()
@@ -11,9 +11,9 @@ return {
       end,
       desc = "FzfLua files (git root)"
     },
-    { "<leader>fg", "<Cmd>FzfLua git_files<CR>",              desc = "FzfLua git files" },
-    { "<leader>,",  "<Cmd>FzfLua buffers<CR>", desc = "FzfLua buffers" },
-    { "<leader>sg", "<Cmd>FzfLua grep<CR>",    desc = "FzfLua grep" },
+    { "<leader>fg", "<Cmd>FzfLua git_files<CR>", desc = "FzfLua git files" },
+    { "<leader>,",  "<Cmd>FzfLua buffers<CR>",   desc = "FzfLua buffers" },
+    { "<leader>sg", "<Cmd>FzfLua grep<CR>",      desc = "FzfLua grep" },
     {
       "<leader>sG",
       function()
@@ -32,22 +32,22 @@ return {
     { "<leader>sf", "<Cmd>FzfLua live_grep<CR>",              desc = "FzfLua live grep (fuzzy)" },
     { "<leader>sb", "<Cmd>FzfLua grep_curbuf<CR>",            desc = "FzfLua fuzzy grep current buffer" },
     { ",",          "<Cmd>FzfLua lgrep_curbuf<CR>",           desc = "FzfLua grep current buffer" },
-    { "<leader>sw", "<Cmd>FzfLua grep_cword<CR>",             mode = { "n" }, desc = "FzfLua current word" },
-    { "<leader>sw", "<Cmd>FzfLua grep_visual<CR>",            mode = { "x" }, desc = "FzfLua grep visual selection" },
+    { "<leader>sw", "<Cmd>FzfLua grep_cword<CR>",             mode = { "n" },                           desc = "FzfLua current word" },
+    { "<leader>sw", "<Cmd>FzfLua grep_visual<CR>",            mode = { "x" },                           desc = "FzfLua grep visual selection" },
     { "<leader>sr", "<Cmd>FzfLua resume<CR>",                 desc = "FzfLua resume" },
     { "<leader>sd", "<Cmd>FzfLua diagnostics_document<CR>",   desc = "Diagnostics document" },
     { "<leader>sk", "<Cmd>FzfLua keymaps<CR>",                desc = "Search keymaps" },
     { "<leader>sh", "<Cmd>FzfLua helptags<CR>",               desc = "Search help" },
     { "<leader>gl", "<Cmd>FzfLua git_bcommits<CR>",           desc = "FzfLua git log (buffer)" },
-    { "gr",         "<cmd>FzfLua lsp_references<cr>",                        desc = "LSP references" },
-    { "gd",         "<cmd>FzfLua lsp_definitions<cr>",                        desc = "LSP definition" },
-    { "gD",         "<cmd>FzfLua lsp_declarations<cr>",                        desc = "LSP declarations" },
-    { "<leader>D",  "<cmd>FzfLua typedefs<cr>",                        desc = "LSP type definition" },
-    { "gi",         "<cmd>FzfLua lsp_implementations<cr>",                  desc = "LSP implementations" },
-    { "ga",         "<cmd>FzfLua lsp_finder<cr>",                  desc = "LSP finder (all)" },
-    { "<leader>ca", "<cmd>FzfLua lsp_code_actions<cr>",                   desc = "LSP document symbols" },
-    { "<leader>ss", "<cmd>FzfLua lsp_document_symbols<cr>",                   desc = "LSP document symbols" },
-    { "<leader>sS", "<cmd>FzfLua lsp_workspace_symbols<cr>",                  desc = "LSP workspace symbols" },
+    { "gr",         "<cmd>FzfLua lsp_references<cr>",         desc = "LSP references" },
+    { "gd",         "<cmd>FzfLua lsp_definitions<cr>",        desc = "LSP definition" },
+    { "gD",         "<cmd>FzfLua lsp_declarations<cr>",       desc = "LSP declarations" },
+    { "<leader>D",  "<cmd>FzfLua typedefs<cr>",               desc = "LSP type definition" },
+    { "gi",         "<cmd>FzfLua lsp_implementations<cr>",    desc = "LSP implementations" },
+    { "ga",         "<cmd>FzfLua lsp_finder<cr>",             desc = "LSP finder (all)" },
+    { "<leader>ca", "<cmd>FzfLua lsp_code_actions<cr>",       desc = "LSP document symbols" },
+    { "<leader>ss", "<cmd>FzfLua lsp_document_symbols<cr>",   desc = "LSP document symbols" },
+    { "<leader>sS", "<cmd>FzfLua lsp_workspace_symbols<cr>",  desc = "LSP workspace symbols" },
   },
   config = function()
     local fzf = require("fzf-lua")
@@ -69,7 +69,7 @@ return {
         border = "rounded",
         preview = {
           hidden = true,
-          scrollchars = { "┃", "" },
+          scrollchars = { "┃", "" }
         },
       },
       oldfiles = {
@@ -95,9 +95,6 @@ return {
       },
       grep = {
         hidden = true,
-        rg_glob = true, -- enable glob parsing
-        glob_flag = "--iglob", -- case insensitive globs
-        glob_separator = "%s%-%-", -- query separator pattern (lua): ' --'
         actions = {
           ["ctrl-i"] = actions.toggle_ignore,
           ["ctrl-h"] = actions.toggle_hidden
@@ -106,10 +103,9 @@ return {
       git = {
         files = {
           formatter = "path.dirname_first",
-        }
-      },
+        },
+      }
     })
     -- fzf.register_ui_select(ui_select)
   end
 }
-
