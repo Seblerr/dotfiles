83cb43761ad782e7fbc56b5da594e66dd667bfbf
Sebastian Nilsson <seblerr@gmail.com>
2026-02-24 08:54:42 +0100

    trigger codediff with now_if_args for mergetool

--- neovim/.config/nvim/plugin/40_misc.lua
+++ neovim/.config/nvim/plugin/40_misc.lua
@@ -1,11 +1,4 @@
-Config.later(function()
-  -- tmux.nvim
-  vim.pack.add({ 'https://github.com/nathom/tmux.nvim' })
-
-  -- render-markdown
-  vim.pack.add({ 'https://github.com/MeanderingProgrammer/render-markdown.nvim' })
-  require('render-markdown').setup({})
-
+Config.now_if_args(function()
   -- codediff
   vim.pack.add({
     'https://github.com/esmuellert/codediff.nvim',
@@ -15,4 +8,13 @@ Config.later(function()
   vim.keymap.set({ 'n', 'x' }, '<leader>gd', '<cmd>CodeDiff file HEAD<cr>', { desc = 'CodeDiff git diff' })
   vim.keymap.set({ 'n', 'x' }, '<leader>gm', '<cmd>CodeDiff file master<cr>',
     { desc = 'CodeDiff git diff against master' })
+end)
+
+Config.later(function()
+  -- tmux.nvim
+  vim.pack.add({ 'https://github.com/nathom/tmux.nvim' })
+
+  -- render-markdown
+  vim.pack.add({ 'https://github.com/MeanderingProgrammer/render-markdown.nvim' })
+  require('render-markdown').setup({})
 end)
